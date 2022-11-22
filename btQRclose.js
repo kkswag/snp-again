b713d05125aa613012ef09aa91fd3c002580bb59
kkswag <kamg_kob@hotmail.co.th>
2022-11-22 16:30:48 +0700

    first commit

---
+++ btQRclose.js
@@ -0,0 +1,11 @@
+// $("button").click(function () {
+//   //   var val = $(".qrContain").css("display");
+
+//   $(".qrContain").addClass("closeQR");
+
+//   console.log("click");
+// });
+
+function FunctionClose() {
+  $(".qrContain").addClass("closeQR");
+}
